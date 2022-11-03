a1fb8a02a0d92dccde9f0cdfbebde4808c3f1d1f
Josehp Herrera <u20211e098@upc.edu.pe>
2022-11-03 11:41:43 -0500

    Proyecto

---
+++ c4-model-design/Program.cs
@@ -0,0 +1,241 @@
+﻿using Structurizr;
+using Structurizr.Api;
+
+namespace c4_model_design
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderModels();
+        }
+
+        static void RenderModels()
+        {
+            const long workspaceId = 77885;
+            const string apiKey = "99233760-a6a4-41eb-a6d0-3333d1e45936";
+            const string apiSecret = "7d8345cd-35a5-4df2-9f0c-0a5d1e1130f4";
+
+            StructurizrClient structurizrClient = new StructurizrClient(apiKey, apiSecret);
+
+            Workspace workspace = new Workspace("Software Design & Patterns - C4 Model - Sistema de comparacion de precios", "UPC Store es un software que se encarga de comparar precios de diversos supermercados");
+
+            ViewSet viewSet = workspace.Views;
+
+            Model model = workspace.Model;
+
+            // 1. Diagrama de Contexto
+            SoftwareSystem monitoringSystem = model.AddSoftwareSystem("UPC Store System", "Permite al usuario comparar precios de productos alimenticios que se encuentran en diferentes supermercados");
+            SoftwareSystem googleMaps = model.AddSoftwareSystem("Google Maps", "Plataforma que ofrece una REST API de información geo referencial.");
+            SoftwareSystem supermercado = model.AddSoftwareSystem("SuperMercado", "Plataforma que ofrece productos alimentarios en ofertas");
+           
+            Person user = model.AddPerson("Usuario", "Usuario comun");
+            Person admin = model.AddPerson("Admin", "User Admin.");
+
+            user.Uses(monitoringSystem, "Realiza consultas para mantenerse al tanto de la planificación de los vuelos hasta la llegada del lote de vacunas al Perú");
+            admin.Uses(monitoringSystem, "Realiza consultas para mantenerse al tanto de la planificación de los vuelos hasta la llegada del lote de vacunas al Perú");
+
+            monitoringSystem.Uses(googleMaps, "Usa la API de google maps");
+            monitoringSystem.Uses(supermercado, "Usa la API de Mercados y tiendas");
+
+            // Tags
+            user.AddTags("Usuario");
+            admin.AddTags("Admin");
+            monitoringSystem.AddTags("SistemaMonitoreo");
+            googleMaps.AddTags("GoogleMaps");
+            supermercado.AddTags("SuperMercado");
+
+            Styles styles = viewSet.Configuration.Styles;
+            styles.Add(new ElementStyle("Usuario") { Background = "#0a60ff", Color = "#ffffff", Shape = Shape.Person });
+            styles.Add(new ElementStyle("Admin") { Background = "#aa60af", Color = "#ffffff", Shape = Shape.Person });
+            styles.Add(new ElementStyle("SistemaMonitoreo") { Background = "#008f39", Color = "#ffffff", Shape = Shape.RoundedBox });
+            styles.Add(new ElementStyle("GoogleMaps") { Background = "#90714c", Color = "#ffffff", Shape = Shape.RoundedBox });
+            styles.Add(new ElementStyle("AircraftSystem") { Background = "#2f95c7", Color = "#ffffff", Shape = Shape.RoundedBox });
+            styles.Add(new ElementStyle("SuperMercado") { Background="#2f95d7",Color="#ffffff",Shape=Shape.RoundedBox});
+
+            SystemContextView contextView = viewSet.CreateSystemContextView(monitoringSystem, "Contexto", "Diagrama de contexto");
+            contextView.PaperSize = PaperSize.A4_Landscape;
+            contextView.AddAllSoftwareSystems();
+            contextView.AddAllPeople();
+
+            // 2. Diagrama de Contenedores
+            Container webApplication = monitoringSystem.AddContainer("Web App", "Permite a los usuarios visualizar un dashboard con el resumen de toda la información del traslado de los lotes de vacunas.", "React");
+            Container landingPage = monitoringSystem.AddContainer("Landing Page", "", "React");
+            Container apiRest = monitoringSystem.AddContainer("API REST", "API Rest", "NodeJS (NestJS) port 8080");
+
+            Container searchContext = monitoringSystem.AddContainer("Search Context", "Bounded Context de Busqueda de supermercados", "NodeJS (NestJS)");
+            Container compareContext = monitoringSystem.AddContainer("Compare Context", "Bounded Context de Comparacion de precios", "NodeJS (NestJS)");
+            Container mappingContext = monitoringSystem.AddContainer("Mapping Context", "Bounded Context de mapeo de los supermercados ", "NodeJS (NestJS)");
+            Container securityContext = monitoringSystem.AddContainer("Security Context", "Bounded Context de Seguridad", "NodeJS (NestJS)");
+
+            Container database = monitoringSystem.AddContainer("Database", "", "Oracle");
+
+            user.Uses(webApplication, "Consulta");
+            user.Uses(landingPage, "Consulta");
+
+            admin.Uses(webApplication, "Consulta");
+            admin.Uses(landingPage, "Consulta");
+
+            webApplication.Uses(apiRest, "API Request", "JSON/HTTPS");
+
+            apiRest.Uses(searchContext, "", "");
+            apiRest.Uses(compareContext, "", "");
+            apiRest.Uses(mappingContext, "", "");
+            apiRest.Uses(securityContext, "", "");
+
+            searchContext.Uses(database, "", "");
+            compareContext.Uses(database, "", "");
+            mappingContext.Uses(database, "", "");
+            securityContext.Uses(database, "", "");
+
+            mappingContext.Uses(googleMaps, "API Request", "JSON/HTTPS");
+            searchContext.Uses(supermercado, "API Request", "JSON/HTTPS");
+            compareContext.Uses(supermercado, "API Request", "JSON/HTTPS");
+
+            // Tags
+            webApplication.AddTags("WebApp");
+            landingPage.AddTags("LandingPage");
+            apiRest.AddTags("APIRest");
+            database.AddTags("Database");
+
+            string contextTag = "Context";
+
+            searchContext.AddTags(contextTag);
+            compareContext.AddTags(contextTag);
+            mappingContext.AddTags(contextTag);
+            securityContext.AddTags(contextTag);
+
+            styles.Add(new ElementStyle("MobileApp") { Background = "#9d33d6", Color = "#ffffff", Shape = Shape.MobileDevicePortrait, Icon = "" });
+            styles.Add(new ElementStyle("WebApp") { Background = "#9d33d6", Color = "#ffffff", Shape = Shape.WebBrowser, Icon = "" });
+            styles.Add(new ElementStyle("LandingPage") { Background = "#929000", Color = "#ffffff", Shape = Shape.WebBrowser, Icon = "" });
+            styles.Add(new ElementStyle("APIRest") { Shape = Shape.RoundedBox, Background = "#0000ff", Color = "#ffffff", Icon = "" });
+            styles.Add(new ElementStyle("Database") { Shape = Shape.Cylinder, Background = "#ff0000", Color = "#ffffff", Icon = "" });
+            styles.Add(new ElementStyle(contextTag) { Shape = Shape.Hexagon, Background = "#facc2e", Icon = "" });
+
+            ContainerView containerView = viewSet.CreateContainerView(monitoringSystem, "Contenedor", "Diagrama de contenedores");
+            contextView.PaperSize = PaperSize.A4_Landscape;
+            containerView.AddAllElements();
+
+            // 3. Diagrama de Componentes (mapping Context)
+            Component domainLayer = mappingContext.AddComponent("Domain Layer", "", "NodeJS (NestJS)");
+            Component mappingController = mappingContext.AddComponent("MappingController", "REST API endpoints de monitoreo.", "NodeJS (NestJS) REST Controller");
+            Component monitoringApplicationService = mappingContext.AddComponent("MonitoringApplicationService", "Provee métodos para la localizacion, pertenece a la capa Application de DDD", "NestJS Component");
+            Component locationRepository = mappingContext.AddComponent("LocationRepository", "Ubicación del supermercado", "NestJS Component");
+
+            apiRest.Uses(mappingController, "", "JSON/HTTPS");
+            mappingController.Uses(monitoringApplicationService, "Invoca métodos de monitoreo");
+
+            monitoringApplicationService.Uses(domainLayer, "Usa", "");
+            monitoringApplicationService.Uses(locationRepository, "", "");
+
+            locationRepository.Uses(database, "", "");
+
+            locationRepository.Uses(googleMaps, "", "JSON/HTTPS");
+
+            // Tags
+            domainLayer.AddTags("DomainLayer");
+            mappingController.AddTags("MonitoringController");
+            monitoringApplicationService.AddTags("MonitoringApplicationService");
+            locationRepository.AddTags("LocationRepository");
+
+            styles.Add(new ElementStyle("DomainLayer") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("MonitoringController") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("MonitoringApplicationService") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("MonitoringDomainModel") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("FlightStatus") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("FlightRepository") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("VaccineLoteRepository") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("LocationRepository") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("AircraftSystemFacade") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+
+            ComponentView componentView1 = viewSet.CreateComponentView(mappingContext, "Components1", "Component Diagram");
+            componentView1.PaperSize = PaperSize.A4_Landscape;
+            componentView1.Add(webApplication);
+            componentView1.Add(apiRest);
+            componentView1.Add(database);
+            componentView1.Add(googleMaps);
+            componentView1.AddAllComponents();
+
+            // 3. Diagrama de Componentes (security Context)
+            Component securityComponent = securityContext.AddComponent("Security Component", "", "NodeJS (NestJS)");
+            Component verifyUser = securityContext.AddComponent("MonitoringApplicationService", "Provee métodos para el monitoreo, pertenece a la capa Application de DDD", "NestJS Component");
+            Component verifyUserRepository = securityContext.AddComponent("verifyUserRepository", "Información del Usuario", "NestJS Component");
+
+            apiRest.Uses(securityComponent, "", "JSON/HTTPS");
+            securityComponent.Uses(monitoringApplicationService, "Invoca métodos de monitoreo");
+
+            monitoringApplicationService.Uses(securityComponent, "Usa", "");
+            monitoringApplicationService.Uses(verifyUser, "", "");
+            monitoringApplicationService.Uses(verifyUserRepository, "", "");
+
+            locationRepository.Uses(database, "", "");
+
+            locationRepository.Uses(googleMaps, "", "JSON/HTTPS");
+
+            // Tags
+            securityComponent.AddTags("securityComponent");
+            verifyUser.AddTags("verifyUser");
+            verifyUserRepository.AddTags("verifyUserRepository");
+
+            styles.Add(new ElementStyle("securityComponent") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("verifyUser") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("verifyUserRepository") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+
+            ComponentView componentView2 = viewSet.CreateComponentView(securityContext, "Components2", "Component Diagram");
+            componentView2.PaperSize = PaperSize.A4_Landscape;
+            componentView2.Add(webApplication);
+            componentView2.Add(apiRest);
+            componentView2.Add(database);
+            componentView2.AddAllComponents();
+
+        
+
+            // 4. Diagrama de Componentes (search Context)
+            Component verifyProduct= searchContext.AddComponent("verifyProduct", "Verifica las coincidencias existentes en los supermercados", "NodeJS (NestJS)");
+            Component supermarketRepository= searchContext.AddComponent("supermarketRepository", "Información de los supermercados", "NestJS Component");
+            Component productController = searchContext.AddComponent("productController", "Comunica la API con el sistema", "NestJS Component");
+            Component dataHandlerSupermarket = searchContext.AddComponent("supermarkerDataHandler", "Información de los supermercados con los productos buscados", "NestJS Component");
+
+
+            searchContext.Uses(monitoringApplicationService, "Invoca métodos de monitoreo");
+            supermarketRepository.Uses(database, "Recupera información");
+            dataHandlerSupermarket.Uses(supermercado, "Recupera información");
+            dataHandlerSupermarket.Uses(supermarketRepository, "Recupera información");
+            dataHandlerSupermarket.Uses(verifyProduct, "usa");
+            productController.Uses(dataHandlerSupermarket, "");
+            verifyProduct.Uses(supermarketRepository, "usa");
+            apiRest.Uses(productController, "JSON/HTTPS");
+
+            monitoringApplicationService.Uses(verifyUser, "", "");
+            monitoringApplicationService.Uses(verifyUserRepository, "", "");
+
+            locationRepository.Uses(database, "", "");
+
+            locationRepository.Uses(googleMaps, "", "JSON/HTTPS");
+
+            // Tags
+            verifyProduct.AddTags("verifyProduct");
+            supermarketRepository.AddTags("supermarketRepository");
+            productController.AddTags("productController");
+            dataHandlerSupermarket.AddTags("dataHandlerSupermarket");
+
+            styles.Add(new ElementStyle("verifyProduct") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("supermarketRepository") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("productController") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("dataHandlerSupermarket") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
+
+            ComponentView componentView3 = viewSet.CreateComponentView(searchContext, "Components3", "Component Diagram");
+            componentView3.PaperSize = PaperSize.A4_Landscape;
+            componentView3.Add(webApplication);
+            componentView3.Add(apiRest);
+            componentView3.Add(database);
+            componentView3.Add(supermercado);
+            componentView3.AddAllComponents();
+
+            structurizrClient.UnlockWorkspace(workspaceId);
+            structurizrClient.PutWorkspace(workspaceId, workspace);
+
+
+        }
+    }
+}
